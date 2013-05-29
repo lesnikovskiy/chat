021b4a723243e41e5d0b049fb3723dbdb94b140f
lesnikovskiy <lesnikovski@gmail.com>
2013-05-29 18:37:47 +0300

    Fixed issue with sessions

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var path = require('path');
 var express = require('express');
 var app = module.exports = express();
 var engine = require('ejs-locals');
+var cookie = require('cookie');
 // good source http://kenny.deeprosoft.com/socket-io-%D0%B8-express-%D1%81%D0%B2%D1%8F%D0%B7%D1%8B%D0%B2%D0%B0%D0%B5%D0%BC-%D0%B2%D1%81%D0%B5-%D0%B2%D0%BC%D0%B5%D1%81%D1%82%D0%B5/
 // github source https://github.com/DanielBaulig/sioe-demo/blob/master/app.js
 var connect = require('express/node_modules/connect');
-var parseCookie = connect.utils.parseCookie;
-var MemoryStore = connect.middleware.session.MemoryStore;
+var MemoryStore = express.session.MemoryStore;
 var store = new MemoryStore();
 
 app.configure(function() {
@@ -19,6 +19,7 @@ app.configure(function() {
 	app.set('views', path.join(__dirname, '/views'));
 	app.set('view engine', 'ejs');
 
+	app.sessionStore = store;
 	app.use(function (req, res, next) {
 		res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 		res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
@@ -35,7 +36,16 @@ app.configure(function() {
 	app.use(express.methodOverride());
 	app.use(express.static(path.join(__dirname, '/public')));
 	app.use(express.cookieParser());
-	app.use(express.session({secret: "qwerty1234567890", key: 'express.sid', store: store = new MemoryStore()}));
+	app.use(express.session({
+		store: app.sessionStore, 
+		secret: 'secret', 
+		key: 'express.sid',
+		cookie: {
+			path: '/',
+			httpOnly: true,
+			maxAge: null
+		}
+	}));
 	app.use(app.router);
 	app.use(express.errorHandler({
 		dumpExceptions: true, showStack: true
@@ -55,7 +65,7 @@ app.get('/', auth, function (req, res) {
 });
 
 app.get('/chat', auth, function (req, res) {
-	res.render('chat', { title: 'Chat'});
+	res.render('chat', { title: 'Web Sockets Demo'});
 });
 
 app.get('/login', function (req, res) {
@@ -63,7 +73,6 @@ app.get('/login', function (req, res) {
 });
 
 app.post('/login', function (req, res) {
-	//res.cookie('express.sid', req.sessionID, {httpOnly: true});
 	req.session.username = req.body.username;
 	res.redirect('/chat');
 });
@@ -73,18 +82,27 @@ var server = http.createServer(app).listen(app.get('port'), function() {
 });
 
 var io = require('socket.io').listen(server).set('authorization', function (data, accept) {	
-	console.log(util.inspect(data.headers));
 	if (!data.headers.cookie)
 		return accept('No cookie transmitted', false);
 		
-	data.cookie = parseCookie(data.headers.cookie);
+	data.cookie = cookie.parse(data.headers.cookie);
 	data.sessionID = data.cookie['express.sid'];
+	data.sessionStore = app.sessionStore;
 	
-	store.load(data.sessionID, function (err, session) {
+	if (data.sessionID) {
+		var exp = /\:(\w+\-*\w+\_*)./gi;
+		var sessionID = exp.exec(data.sessionID)[1];
+	}
+	
+	if (!sessionID)
+		return data('Couldn\'t parse session ID', false);
+	
+	app.sessionStore.get(sessionID, function (err, session) {		
 		if (err || !session) 
 			return accept('Error', false);
 			
-		data.session = session;
+		data.session = new express.session.Session(data, session);
+		
 		return accept(null, true);
 	});
 });
@@ -93,9 +111,10 @@ io.set('log level', 1);
 
 io.sockets.on('connection', function(socket) {	
 	var hs = socket.handshake.session;
+	console.log(util.inspect(hs));
 	
 	// for the simplicity use first 5 symbols of user as ID
-	var ID = (socket.id).toString().substr(0, 5);
+	var ID = hs.username ? hs.username : (socket.id).toString().substr(0, 5);
 	var time = (new Date()).toLocaleTimeString();
 	// send client message about successful connection
 	socket.json.send({'event': 'userJoined', 'name': ID, 'time': time});
@@ -112,6 +131,7 @@ io.sockets.on('connection', function(socket) {
 	});
 	
 	socket.on('set_session', function(data) {
+		console.log('set_session called with param: ' + data);
 		hs.reload(function() {
 			hs.value = data;
 			hs.touch().save();
